fe4ec737194f06f0381d77db437865d1faab9a34
aanandigoel35-lab <aanandigoel35@gmail.com>
2026-02-16 11:54:34 +0530

    Day 15 Q1.c
    This program reads a matrix of size m x n and calculates the sum of its primary diagonal elements.

---
+++ Day 15 Q1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int sum = 0;
+    int matrix[m][n];
+
+    // Read matrix elements
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Calculate primary diagonal sum
+    int limit = (m < n) ? m : n;
+    for(int i = 0; i < limit; i++) {
+        sum += matrix[i][i];
+    }
+
+    // Print result
+    printf("%d\n", sum);
+
+    return 0;
+}
